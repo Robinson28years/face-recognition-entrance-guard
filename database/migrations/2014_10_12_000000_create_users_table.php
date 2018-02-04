0725ea5e3c278fa708b4b8000e1d0bf601e3e175
Robinson28years <bb.chen@outlook.com>
2018-02-04 13:48:11 +0800

    用户表与控制层

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('name')->nullable();;
+            $table->string('email')->nullable();;
+            $table->string('phone')->nullable();;
+            $table->uuid('face_id')->nullable();;
+            $table->string('password')->nullable();;
             $table->rememberToken();
             $table->timestamps();
         });
