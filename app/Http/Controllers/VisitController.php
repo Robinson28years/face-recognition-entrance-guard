bf6c5e80299f68ab4d2bb294900ad42e611e8abf
Robinson28years <bb.chen@outlook.com>
2018-02-20 21:34:58 +0800

    访问记录准备

--- app/Http/Controllers/VisitController.php
+++ app/Http/Controllers/VisitController.php
@@ -7,79 +7,85 @@ use Illuminate\Http\Request;
 
 class VisitController extends Controller
 {
+
     /**
-     * Display a listing of the resource.
+     * 列出某用户所有的地址以及角色和时间段
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function addressIndex(User $user)
     {
-        //
+        // dd($user->addresses);
+        return new UserAddressCollection($user->addresses);
     }
 
     /**
-     * Show the form for creating a new resource.
+     * 列出某用户在某地址拥有的角色和时间段
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function userAddress(User $user,Address $address)
     {
-        //
+        $address2 = $user->addresses()->find($address->id);
+        // dd($address2);
+        return new UserAddressResource($address2);
     }
 
     /**
-     * Store a newly created resource in storage.
+     * 列出某地址所有的用户以及角色和时间段
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function userIndex(Address $address)
+    {
+        // dd($address->users);
+        return new AddressUserCollection($address->users);
+        // return new UserAddressCollection($address->users);
+    }
+
+    /**
+     * 为地址注册用户并绑定权限和时间
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+
+    public function store(Request $request,Address $address)
     {
-        //
+        $address->users()->attach($request->user_id,[
+            'role_id' => $request->role_id,
+            'time' => $request->time,
+        ]);
+
+        $user = User::find($request->user_id);
+        $address2 = $user->addresses()->find($address->id);
+        return new UserAddressResource($address2);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Visit  $visit
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Visit $visit)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Visit  $visit
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Visit $visit)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
+     /**
+     * 更新绑定权限和时间
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Visit  $visit
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Visit $visit)
+
+    public function update(Request $request,User $user,Address $address)
     {
-        //
-    }
+        $user->addresses()->updateExistingPivot($address->id,$request->all());
+        $address2 = $user->addresses()->find($address->id);
+        return new UserAddressResource($address2);
+    }   
 
     /**
-     * Remove the specified resource from storage.
+     * 删除某用户与地址的绑定
      *
-     * @param  \App\Visit  $visit
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Visit $visit)
+    public function destroy(User $user,Address $address)
     {
-        //
+        $user->addresses()->detach($address->id);
+        return response()->json([
+            'success' => true,
+        ]);
     }
 }
