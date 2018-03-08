66b0720868db783dc007d431e7980d1062755ee4
Robinson28years <bb.chen@outlook.com>
2018-03-08 16:12:47 +0800

    尝试跨域

--- routes/api.php
+++ routes/api.php
@@ -124,4 +124,6 @@ Route::post('auth/visit/{building}','VisitController@auth');
 
 Route::post('auth/{building}','FaceController@auth');
 
+Route::post('visits/test','UserAddressController@test');
+
 });
