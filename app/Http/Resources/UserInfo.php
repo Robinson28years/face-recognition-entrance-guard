cd1da4ac91b9a5a65f22c413b36980b89ee6a43a
Robinson28years <bb.chen@outlook.com>
2018-03-30 14:12:56 +0800

    返回用户数据修改

--- app/Http/Resources/UserInfo.php
+++ app/Http/Resources/UserInfo.php
@@ -7,6 +7,7 @@ use App\Http\Resources\Address as AddressResource;
 use App\Http\Resources\AddressCollection;
 use App\Http\Resources\AddressUser as AddressUserResource;
 use App\Address;
+use App\UserAddress;
 
 class UserInfo extends Resource
 {
@@ -19,14 +20,29 @@ class UserInfo extends Resource
     public function toArray($request)
     {
         $roles = array();
+        $flag=false;
+        $address=null;
         foreach($this->roles as $role) {
+            if($role->name == 'owner'){
+                $flag = true;
+            }
             array_push($roles,$role->name);
         };
+        if($flag){
+            $userAddress = UserAddress::where(['user_id'=>$this->id,'role_id'=>5])->first();
+            $address = Address::find($userAddress->address_id);
+            // dd($address);
+        }
+        // dd($address);
         return [
             'id' => $this->id,
             'name' => $this->name,
             'phone' => $this->phone,
             'roles' => $roles,
+            // 'address' => new AddressResource($address),
+            'address' => $this->when($flag , function ()use($address) {
+                return new AddressResource($address);
+            }),
             // 'addresses' => AddressUserResource::collection($this->addresses),
         ];
     }
