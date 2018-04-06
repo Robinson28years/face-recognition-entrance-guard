322177065ab68f9188db9a78f23b9e9933f794cb
Robinson28years <bb.chen@outlook.com>
2018-04-06 14:55:26 +0800

    构建前端

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/property', function () {
+    return view('property');
+});
+
+Route::get('/owner', function () {
+    return view('owner');
+});
